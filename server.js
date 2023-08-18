e0ce833a80f244cded3eae3c39c8b717d9b8a453
HichTala <98521878+HichTala@users.noreply.github.com>
2023-08-18 13:20:16 +0200

    Add console logging

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.log('Starting the server...');
 var http = require('http')
 var fs = require('fs')
 
@@ -6,8 +7,11 @@ const PORT = 8080
 fs.readFile('./html/main.html', function(error, html) {
    if (error) throw error;
    http.createServer(function(request, response) {
+       console.log('Request received');
        response.writeHeader(200, {"Content-Type": "text/html"});
        response.write(html);
        response.end();
-   }).listen(PORT)
+   }).listen(PORT, function() {
+       console.log(`Server running at http://localhost:${PORT}`);
+   });
 });
