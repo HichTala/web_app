5b431ceba0fa0681b8aa51f110daae580494d55b
HichTala <98521878+HichTala@users.noreply.github.com>
2023-08-18 15:58:00 +0200

    Add CSS and javascript

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 console.log('Starting the server...');
-var http = require('http')
-var fs = require('fs')
+var http = require('http');
+var fs = require('fs');
+var path = require('path'); // Import the path module for working with file paths
 
-const PORT = 8080
+const PORT = 8080;
 
-fs.readFile('./html/main.html', function(error, html) {
-   if (error) throw error;
-   http.createServer(function(request, response) {
-       console.log('Request received');
-       response.writeHeader(200, {"Content-Type": "text/html"});
-       response.write(html);
-       response.end();
-   }).listen(PORT, '0.0.0.0', function() {
-       console.log(`Server running at http://localhost:${PORT}`);
-   });
+http.createServer(function(request, response) {
+    console.log('Request received');
+
+    // Parse the URL to determine which file to serve
+    var filePath = '.' + request.url;
+    if (filePath === './') {
+        filePath = './html/main.html'; // Default to serving main.html
+    }
+
+    // Get the file extension to set the appropriate Content-Type header
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+    };
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                response.writeHead(404, {'Content-Type': 'text/plain'});
+                response.end('File not found');
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+            }
+        } else {
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content);
+        }
+    });
+
+}).listen(PORT, function() {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
