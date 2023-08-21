64d2ea09e0cbfcccbed9389e9c19c142b9018fda
HichTala <98521878+HichTala@users.noreply.github.com>
2023-08-21 12:52:03 +0200

    Remove run python from client side app

--- js/main.js
+++ js/main.js
@@ -1,35 +1,7 @@
 function openForm() {
   document.getElementById("formContainer").style.display = "block";
-
-  let form = document.getElementById("myForm")
-
-  // form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
-  //
-  //   let client = document.getElementById("client");
-  //   let commande = document.getElementById("commande");
-  //   let montant = document.getElementById("montant");
-  //
-  //   console.log(client.value)
-  //   console.log(commande.value)
-  //   console.log(montant.value)
-  //
-  //   // runPython(client.value, commande.value, montant.value)
-  // });
-
 }
 
 function closeForm() {
   document.getElementById("formContainer").style.display = "none";
-}
-
-function runPython(client, commande, montant) {
-  console.log(client)
-  console.log(commande)
-  console.log(montant)
-
-  const execSync = require('child_process').execSync;
-
-  const output = execSync('python3 ../python/main.py ${client} ${commande} ${montant}', { encoding: 'utf-8' });
-  console.log('Output was:\n', output);
 }
